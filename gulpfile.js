1dde42a11facdb6766fe90af9627274b8ef2385a
Endis UK <paul.tostevin@endis.com>
2016-02-23 15:05:55 +0000

    minor updates

--- gulpfile.js
+++ gulpfile.js
@@ -16,89 +16,95 @@ var env,
     sassSources,
     htmlSources,
     outputDir,
+    lineComments,
     sassStyle;
 
 env = 'development';
 
 if (env==='development') {
-  outputDir = 'builds/development/';
-  sassStyle = 'expanded';
+    outputDir = 'builds/development/';
+    sassStyle = 'expanded';
+    lineComments = true;
+
 } else {
-  outputDir = 'builds/production/';
-  sassStyle = 'compressed';
+    outputDir = 'builds/production/';
+    sassStyle = 'nested';
+    lineComments = false;
 }
 
 jsSources = [
-  'components/scripts/jquery.bxslider.min.js',
-  'components/scripts/jquery.slicknav.min.js'
+    'components/scripts/jquery.bxslider.min.js',
+    'components/scripts/jquery.slicknav.min.js'
 ];
 
 sassSources = ['components/sass/style.scss'];
 htmlSources = [outputDir + '*.html'];
 
 gulp.task('js', function() {
-  'use strict';
+    'use strict';
 
-  gulp.src('components/scripts/script.js')
-    .pipe(jshint('./.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulpif(env === 'production', uglify()))
-    .pipe(gulp.dest(outputDir + 'js'));
+    gulp.src('components/scripts/script.js')
+        .pipe(jshint('./.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest(outputDir + 'js'));
 
 
-  gulp.src(jsSources)
-    .pipe(concat('plugins.js'))
-    .pipe(browserify())
-    .on('error', gutil.log)
-    .pipe(gulpif(env === 'production', uglify()))
-    .pipe(gulp.dest(outputDir + 'js'))
-    .pipe(connect.reload());
+    gulp.src(jsSources)
+        .pipe(concat('plugins.js'))
+        .pipe(browserify())
+        .on('error', gutil.log)
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest(outputDir + 'js'))
+        .pipe(connect.reload());
 });
 
 gulp.task('compass', function() {
-  'use strict';
-  gulp.src(sassSources)
-    .pipe(compass({
-      sass: 'components/sass',
-      css: outputDir + 'css',
-      image: outputDir + 'images',
-      style: sassStyle,
-      require: ['susy', 'breakpoint']
-    })
-    .on('error', gutil.log))
-    // .pipe(gulp.dest( outputDir + 'css'))
-    .pipe(connect.reload());
+    'use strict';
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            css: outputDir + 'css',
+            image: outputDir + 'images',
+            style: sassStyle,
+            comments: lineComments,
+            require: ['susy', 'breakpoint']
+        })
+            .on('error', gutil.log))
+        // .pipe(gulp.dest( outputDir + 'css'))
+        .pipe(connect.reload());
 });
 
+
+
 gulp.task('watch', function() {
-  'use strict';
-  gulp.watch(jsSources, ['js']);
-  gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);
-  gulp.watch('builds/development/*.html', ['html']);
+    'use strict';
+    gulp.watch(jsSources, ['js']);
+    gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);
+    gulp.watch('builds/development/*.html', ['html']);
 });
 
 gulp.task('connect', function() {
-  'use strict';
-  connect.server({
-    root: outputDir,
-    livereload: true
-  });
+    'use strict';
+    connect.server({
+        root: outputDir,
+        livereload: true
+    });
 });
 
 gulp.task('html', function() {
-  'use strict';
-  gulp.src('builds/development/*.html')
-    .pipe(gulpif(env === 'production', minifyHTML()))
-    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
-    .pipe(connect.reload());
+    'use strict';
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+        .pipe(connect.reload());
 });
 
 // Copy images to production
 gulp.task('move', function() {
-  'use strict';
-  gulp.src('builds/development/images/**/*.*')
-  .pipe(gulpif(env === 'production', gulp.dest(outputDir+'images')));
+    'use strict';
+    gulp.src('builds/development/images/**/*.*')
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir+'images')));
 });
 
 gulp.task('default', ['watch', 'html', 'js', 'compass', 'move', 'connect']);
-
